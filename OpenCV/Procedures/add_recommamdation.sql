f4c629646e9a85897b6dece7a38fa135e2d3a0c0
pokemfeze <maximefeze1@gmail.com>
2023-01-06 13:53:49 +0100

    ajout de la procedure add de la table

---
+++ OpenCV/Procedures/add_recommamdation.sql
@@ -0,0 +1,33 @@
+DECLARE 
+
+v_id_user   Users.id_user%type;  
+
+begin 
+ 
+select r.id_user
+into v_id_user
+from Users u
+join  Recommandation r 
+on r.id_user = u.id_user
+where name_user='&Votre_Nom';
+
+    insert into Recommandation 
+    (
+        id_recom,
+        personName,
+        relationShipLevel,
+        postAtTheTime,
+        message,
+        id_user
+    )
+    values
+    (
+     seq_recom.nextval,
+	'&personName',
+	'&relationShipLevel',
+    '&postAtTheTime',
+    '&message',
+     v_id_user
+     );
+end ;
+/
