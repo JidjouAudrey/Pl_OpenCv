9f208f473190894cec2df281f2e403981cdba93d
cabrel sianou <sianou93@gmail.com>
2023-01-06 16:44:45 +0100

    modification de la procedure edite experience pro

--- OpenCV/Procedures/interface_edit/procedure_edit/edite_experiencePro.sql
+++ OpenCV/Procedures/interface_edit/procedure_edit/edite_experiencePro.sql
@@ -7,6 +7,13 @@ select id_expro
 into   v_id_exp
 from   Professional_exp
 WHERE status_expro= '&status_de_lexperience';
+UPDATE Professional_exp
+SET 
+status_expro='&new_status_expro',
+startDate ='&new_startDate',
+endDate='&new_endDate',
+organisation='&new_organisation',
+function_exp= '&new_function_exp'
 
 
 WHERE id_expro =v_id_exp;
